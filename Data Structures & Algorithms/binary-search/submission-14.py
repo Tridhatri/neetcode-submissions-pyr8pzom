c6d75bd9eafe4096c007c07a38969e657d8de90b
Tridhatri <105197746+Tridhatri@users.noreply.github.com>
2026-04-15 14:45:55 +0530

    Add: binary-search - submission-14

---
+++ Data Structures & Algorithms/binary-search/submission-14.py
@@ -0,0 +1,19 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        # Binary search using recursive approach
+        # using end indices
+        def binary_search(left, right):
+            mid = (left + right) //2
+            if left > right:
+                return -1
+            if target == nums[mid]:
+                return mid
+            elif target > nums[mid]:
+                # Right side
+                return binary_search(mid+1,right)
+            else:
+                return binary_search(left,mid-1)
+
+        return binary_search(0, len(nums)-1)
+
+        
